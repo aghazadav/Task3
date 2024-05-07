602643592731063624eda92da2acc8092c2c5e38
aghazadav <cmdjarrier@hotmail.com>
2024-05-07 15:24:07 +0400

    Tasks are done

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Əgər   a və b  ədədləri 2 və 4-ə bölünürsə, onların 8-ə də bölünməsini müəyyən etmək üçün proqram tərtib etməli
+             */
+            Console.WriteLine($"Enter numbers a and b:");
+            Console.WriteLine();
+            Console.Write("a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (a % 2 == 0 && a % 4 == 0 && b % 2 == 0 && b % 4 == 0)
+            {
+                Console.WriteLine($" {a} and {b} are divisible by 2 and 4, and also by 8.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} and {b} do not meet the conditions.");
+            }
+        }
+    }
+}
---
+++ Lesson6/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                 Verilmiş  a ,b və c tərəflərinə görə üçbucaq qurmağın mümkünlüyünü və həmin üçbucağın bərabəryanlı,
+             bərabərtərəfli və ya müxtəliftərəfli olmasını yoxlamalı
+             */
+
+            Console.WriteLine($"Enter numbers side a, side b and side c for triangel:");
+            Console.WriteLine();
+            Console.Write("Side a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Side b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Side c: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (a + b > c && c + b > a && a + c > b)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Creating triangle is possible");
+                if (a == b && b == c)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Equilateral triangle created");
+                }
+                else if (a == b || b == c || c == a)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Isosceles triangle created");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Scalane triangle created");
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Creating triangle not possible");
+            }
+
+            
+        }
+    }
+}
